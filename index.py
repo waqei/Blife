54840e621adb9ee2c9e8c524ae2f8c20f9f1caa7
zhwei <sdutzhangwei@gmail.com>
2012-11-11 17:19:26 +0800

    11.11

--- index.py
+++ index.py
@@ -1,9 +1,26 @@
 #encoding=utf-8
+import web
+
+#urls
+from web.template import render
 
 urls=(
-    '/','index'
+		'/','index'
 )
+
+#application
+#app = web.application(urls, globals())
+
+#templates
+index = web.template.frender('index/index.html')
+#admin = web.template.frender('admin/admin.html')
+name="hello"
 
 class index:
     def GET(self):
-        return 
+        return render.index(None)
+
+
+if __name__ == "__main__":
+    app = web.application(urls,globals())
+    app.run()
