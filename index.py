58d68bf3a992378c485373f4911cf7bc6d8df49b
zhwei <sdutzhangwei@gmail.com>
2012-11-11 23:03:20 +0800

    admin

--- index.py
+++ index.py
@@ -1,26 +1,33 @@
 #encoding=utf-8
 
 import web
-import sae
+#import sae
 from web.template import render
 
 #urls
 urls=(
-		'/','index'
-		'categories','catego'
+		'/','index',
+		'/categories','catego',
+		'/admin','admin'
 )
 
 
 #templates
 render = web.template.render('index/')
-#admin = web.template.frender('admin/admin.html')
+admin_render=web.template.render('admin/')
 
+#静态页返回
 class index:
     def GET(self):
         return render.index()
+
 class catego:
 	def GET(self):
 		return render.categories()
+
+class admin:
+	def GET(self):
+		return admin_render.admin()
 
 if __name__ == "__main__":
     app = web.application(urls,globals())
